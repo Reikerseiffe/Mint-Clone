75940f29620466025068c843570d3c1be60e8092
Reiker Seiffe <reikerseiffe@gmail.com>
2020-03-19 00:42:12 -0500

    -Added the ability to get deposits -Changes getDebitsForMonth to be a generic dataframe for month function - Added nicer formatting

--- mint.py
+++ mint.py
@@ -13,24 +13,40 @@ masterFrame = pd.read_csv(CSV_LOCATION)
 masterFrame.Date = pd.to_datetime(masterFrame.Date)
 
 
+
+def getDeposits(dataToParse):
+    allDeposits = dataToParse[
+        (dataToParse['Transaction Type'] == 'credit') & 
+        (dataToParse['Category'] != 'Credit Card Payment') &
+        (dataToParse['Category'] != 'Transfer') &
+        (dataToParse['Description'] != 'Autosave Payday Savings')
+        ]
+    print(allDeposits.sort_values(['Amount'], ascending=False))
+    print("\n\n")
+    print("Total Amount of Deposits: " + str(len(allDeposits.index)))
+    print("Total Earned: " + str(allDeposits['Amount'].sum()))
+
+
+
 #Parse down dataframe for the month requested
-def getDebitsForMonth(year, month):
+def getDataFrameForMonth(year, month):
     numberOfDaysInMonth = str(monthrange(year, month)[1])
     year = str(year)
     month = str(month)
     parsedData = masterFrame[(masterFrame['Date'] >= year + '-' + month + '-01') & (masterFrame['Date'] <= year + '-' + month + '-' + numberOfDaysInMonth)]
-
-    getDebits(parsedData)
+    return parsedData
 
 #Find all debits in a dataframe that are not credit card payments or transfers between accounts
 def getDebits(dataToParse):
     allDebits = dataToParse[(dataToParse['Transaction Type'] == 'debit') & (dataToParse['Category'] != 'Credit Card Payment') & (dataToParse['Category'] != 'Transfer')]
     print(allDebits.sort_values(['Amount'], ascending=False))
+    print("\n\n")
     print("Total Amount of Debits: " + str(len(allDebits.index)))
     print("Total Spent: " + str(allDebits['Amount'].sum()))
 
 
 #Return sample for March of 2020
 print("March: ")
-getDebitsForMonth(2020,3)
-
+getDebits(getDataFrameForMonth(2020,3))
+print("\n\n****************************\n\n")
+getDeposits(getDataFrameForMonth(2020,3))
